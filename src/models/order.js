5836e861ff401d571485a03a648dc1adb8102a40
İrem Kurt <iremkurt19@gmail.com>
2022-07-23 18:24:10 +0300

    completed cart&order model

---
+++ src/models/order.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const orderSchema = new mongoose.Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer',
+    },
+    orderData: {
+        type: Data,
+        required: true,
+        default: Date.now
+    },
+
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
+    orderStatus: {
+        type: String, 
+        required: true, 
+        default: 'pending', 
+        enum: ['pending', 'completed', 'cancelled']
+    },
+    orderItems: [{
+        item: {type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'},
+        quantity: {
+            type: Number,
+            required: true}
+
+        }],
+    notes: {
+        type: String,}
+
+})
+module.exports = mongoose.model('Order', orderSchema);
+
+// const mongoose = require('mongoose');
+
+// const orderSchema = mongoose.Schema({
+//     total: {  
+//         type: Number, 
+//         required: true 
+//     },
+//     shopItems: {
+//         type: [{
+//             shopItemId: { 
+//                 type: mongoose.Schema.Types.ObjectId,
+//                 ref: 'Product',
+//             },
+//             quantity: {
+//                 type: Number,
+//                 required: true
+//             }
+//         }],
+//     default: []
+// },
+// customerRef: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Customer'
+// }
+// })
+
+// module.exports = mongoose.model('Order', orderSchema);
