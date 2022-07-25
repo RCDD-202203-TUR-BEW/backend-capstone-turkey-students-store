4a3d62e33e0449349ec9232da52a01a32a294d6b
Israa Qaba <israa.qaba98@gmail.com>
2022-07-25 08:56:02 +0300

    Feat: add winston logger file and port environment variable  #2

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ const express = require('express');
 require('express-async-errors');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error');
+const logger = require('./utils/Logger');
+require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.NODE_LOCAL_PORT || 3000;
 
 app.use('/api', routes);
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('listening on port 3000');
+  logger.error('listening on port 3000');
 });
---
+++ src/utils/Logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({
+      filename: 'error.log',
+      level: 'error',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
