6fd8ebf0df1b31cf3fe076e7f66cb3a199108c0a
melekarslan13 <melekarslan.ie@gmail.com>
2022-07-30 15:44:40 +0000

    feat:swagger integration is done

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('express-async-errors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 const routes = require('./routes');
 const logger = require('./utils/logger');
 const errorHandler = require('./middlewares/error');
@@ -13,6 +15,8 @@ const port = process.env.PORT || 3000;
 app.use('/api', routes);
 
 app.use(errorHandler);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   logger.info('[+] listening on port 3000');
