3101af520db4b85326b3db7fb6c85cf7510dece3
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-08-02 00:45:35 +0300

    refactor: unnecessary comments and code deleted

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const authenticate = require('../middlewares/authenticate');
+// const authenticate = require('../middlewares/authenticate');
 
 const authController = require('../controllers/auth');
 
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
-router.get('/signout', authenticate.verifyUser, authController.signout);
 
 module.exports = router;
