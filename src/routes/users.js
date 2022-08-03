97c5bde2511ee1d40d35708ab4dc9efe8151111a
melekarslan13 <melekarslan.ie@gmail.com>
2022-08-03 04:09:59 +0300

    feat:patch endpoint added

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,5 +4,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const usersController = require('../controllers/users');
 
 router.get('/', usersController.getUser);
+router.patch('/products/:productId', usersController.updateProduct);
+//  router.patch('/products/:productId', isLogged, isAdmin,usersController.updateProduct)
 
 module.exports = router;
