0787f685c50f8d70d5bb127012237bef4d32f89b
melekarslan13 <melekarslan.ie@gmail.com>
2022-08-08 10:21:54 +0300

    Implement PATCH /api/products/:productId endpoint

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -2,6 +2,29 @@ const { body, validationResult } = require('express-validator');
 const ErrorResponse = require('../utils/errorResponse');
 const Product = require('../models/product');
 
+exports.createProduct = async (req, res) => {
+  console.log('REQ.BODY:', req.body);
+  try {
+    const product = await Product.create({
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      coverImage: req.body.coverImage,
+      images: req.body.images,
+      type: req.body.type,
+      location: req.body.location,
+      status: req.body.status,
+      condition: req.body.condition,
+    });
+
+    return res.status(201).json({ success: true, data: product });
+  } catch (error) {
+    console.log('ERROR:', error);
+    return res.status(500).json(error);
+  }
+};
+
 exports.updateProduct = async (req, res) => {
   body('title').isLength({ max: 150 });
   body('description').isEmpty();
@@ -29,6 +52,7 @@ exports.updateProduct = async (req, res) => {
         location: req.body.location,
         status: req.body.status,
         condition: req.body.condition,
+        seller: req.body.seller,
       },
     },
     { new: true }
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,5 +4,6 @@ const ErrorResponse = require('../utils/errorResponse');
 const productsController = require('../controllers/products');
 
 router.patch('/:id', productsController.updateProduct);
+router.post('/', productsController.createProduct);
 
 module.exports = router;
