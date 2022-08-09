c62ee2bfdf798ae9b67496a62ae38bc2d38d0135
mohammad.si.tt@gmail.com <mohammad.si.tt@gmail.com>
2022-08-09 13:22:03 +0300

    fix conflicts solved fixes #14

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 require('express-async-errors');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const cookieParser = require('cookie-parser');
 const { encryptCookieNodeMiddleware } = require('encrypt-cookie');
 const { expressjwt: jwt } = require('express-jwt');
 require('dotenv').config();
-require('./middlewares/passport.config');
+// require('./middlewares/passport.config');
+// require('./middlewares/passport-auth');
+require('./services/passport.config');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
@@ -27,9 +28,6 @@ app.use(
   })
 );
 const { connectToMongoAtlas } = require('./db/connection');
-const User = require('./models/user');
-
-connectToMongoAtlas();
 
 app.use(express.json());
 
@@ -37,8 +35,6 @@ const port = process.env.PORT || 3000;
 
 app.use(cookieParser(process.env.SECRET_KEY));
 app.use(encryptCookieNodeMiddleware(process.env.SECRET_KEY));
-
-require('./middlewares/passport-auth');
 
 app.use(passport.initialize());
 
