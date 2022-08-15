4ac1d75d6d01bfc9eaba5c78a8dacc80bb9d18d5
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-08-15 22:43:56 +0300

    feat: add documentation for GET /api/orders/ endpoint

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -4,14 +4,3 @@ exports.getMyOrders = async (req, res, next) => {
   const myOrders = await Order.find({ buyer: req.user._id });
   return res.status(200).json({ success: true, data: myOrders });
 };
-
-// DELETE AFTER TESTING
-exports.giveOrder = async (req, res, next) => {
-  const order = await Order.create({
-    buyer: req.user._id,
-    totalPrice: 100,
-    orderItems: [],
-    notes: 'My books order',
-  });
-  return res.status(201).json({ success: true, data: order });
-};
