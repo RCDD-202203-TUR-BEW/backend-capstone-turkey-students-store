d29759f642466585ab465b054735422c4d805779
Muslem Omar <mislimumer@gmail.com>
2022-08-22 16:42:58 +0300

    fix: enable cors for specific origins

--- src/app.js
+++ src/app.js
@@ -20,10 +20,24 @@ connectToMongoAtlas();
 const app = express();
 
 const port = process.env.PORT || 3000;
+const allowedOrigins = ['http://localhost:3000'];
 
-// use cors
-app.use(cors());
+const corsOptions = {
+  origin(origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
