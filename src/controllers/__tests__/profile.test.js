3e5c12f5198956d3ca48051c265dff8e01f4c6ef
melekarslan13 <melekarslan.ie@gmail.com>
2022-08-25 14:09:43 +0300

    feat:Implement PATCH /api/profile/ endpoint

--- src/controllers/__tests__/profile.test.js
+++ src/controllers/__tests__/profile.test.js
@@ -222,3 +222,67 @@ describe('Products routes', () => {
     });
   });
 });
+describe('User routes', () => {
+  const myUser = {
+    firstName: 'Melek',
+    lastName: 'Arslan',
+    email: 'melek@mail.com',
+    schoolName: 'Bilkent University',
+    password: 'Ma123',
+  };
+  beforeEach(async () => {
+    await server.post('/api/auth/signup').send(myUser);
+  });
+
+  describe('PATCH /', () => {
+    test('If user email exists, should return an error with status code 400', async () => {
+      const expectedResponse = {
+        success: false,
+        error: 'Email already exists!',
+      };
+      const user = {
+        firstName: 'Mike',
+        lastName: 'Arslran',
+        email: 'melek@mail.com',
+        schoolName: 'Başkent University',
+      };
+      const res = await server.patch('/api/profile/').send(user);
+      expect(res.status).toBe(400);
+      expect(res.headers['content-type']).toMatch('application/json');
+      expect(res.body).toEqual(expect.objectContaining(expectedResponse));
+    });
+    test('If old password is not correct , should return an error with status code 401', async () => {
+      const expectedResponse = {
+        success: false,
+        error: 'Your old password is not correct please enter again!',
+      };
+      const user = {
+        firstName: 'Mikey',
+        oldPassword: 'Ma123445',
+        password: 'Ma123w',
+      };
+      const res = await server.patch('/api/profile/').send(user);
+      expect(res.status).toBe(401);
+      expect(res.headers['content-type']).toMatch('application/json');
+      expect(res.body).toEqual(expect.objectContaining(expectedResponse));
+    });
+    test('If user data is valid, should update user, return its data with status code 200', async () => {
+      const mValidRequest = {
+        firstName: 'Mike',
+      };
+
+      const expectedResponse = {
+        firstName: 'Mike',
+        lastName: 'Arslan',
+        email: 'melek@mail.com',
+        schoolName: 'Bilkent University',
+      };
+
+      const res = await server.patch('/api/profile/').send(mValidRequest);
+      expect(res.status).toBe(200);
+      expect(res.headers['content-type']).toMatch('application/json');
+      expect(res.body.success).toBe(true);
+      expect(res.body.data).toEqual(expect.objectContaining(expectedResponse));
+    });
+  });
+});
