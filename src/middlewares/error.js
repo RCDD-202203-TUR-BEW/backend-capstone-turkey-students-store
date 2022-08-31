19be3febaf986cd30a36b499f2c34d9cf821fc05
Muslem Omar <mislimumer@gmail.com>
2022-08-31 16:07:45 +0300

    log errors

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,4 +1,9 @@
+const logger = require('../utils/logger');
+
 const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  logger.error(err.message, err);
+
   res.status(err.statusCode || 500).json({
     success: false,
     error: err.message || 'Server Error',
